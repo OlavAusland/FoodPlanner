20c9f005fe5598c01a153d38df16ddbeaf0bf70f
Olav Ausland Onstad <olavao@uia.no>
2020-10-15 11:46:35 +0200

    Added .JSON database for meals

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import random
 
+class Recipe:
+    def __init__(self, name):
+        self.name = name
+        self.ingredients = list()
+
+    def add(self, ingredients: list()):
+        for ingredient in ingredients:
+            self.ingredients.append(ingredient)
+
+
+
+
 def main():
     print(random.randint(0, 10))
 
